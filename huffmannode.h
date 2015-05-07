7ed98a85f5f98706c09a06d213ff2c155358567b
kidynamit <paul.mwanjohi@gmail.com>
2015-05-07 11:00:10 +0200

    first commit

---
+++ huffmannode.h
@@ -0,0 +1,13 @@
+#ifndef __NODE
+#define __NODE
+
+class HuffmanNode {
+    private:
+        HuffmanNode * left, right;
+        char letter;
+        unsigned int frequency;
+    public:
+        Huffman 
+};
+
+#endif
